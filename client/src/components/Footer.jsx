55e34e317b493c849f42c807d05777bfed9c74d1
polentzisb <polentzi.sb1@gmail.com>
2023-01-21 09:45:06 -0300

    first

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../img/logo.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+      <p>&copy; Polentzi Saavedra</p><b>Blue Apron Blog 2023 🥕</b>
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
